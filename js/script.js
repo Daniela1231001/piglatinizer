1588096cd850acd0c68e6e290240b9cc38c5bcfd
Daniela1231001 <Dramos1001.student@hsfi.us>
2019-11-06 23:26:49 +0000

    NO changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("#button").click(function(){
+    let input = $(".input").val();
+    let answer = input;
+
+    
+
+});
 
 
 
-  
-  
-  
-  
+
+
 });
